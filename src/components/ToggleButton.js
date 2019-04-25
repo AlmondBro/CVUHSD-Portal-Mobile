47a261cbbeefc587488a4140302a6d1eb1891c64
JuanDavidLopez95 <juandavid@jdlondono.com>
2019-04-25 08:41:50 -0700

    Modify SDK version in app.json to remove warning. Finish styling basic toggle button.

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -5,7 +5,18 @@ import { View, StyleSheet, TouchableHighlight, Text } from "react-native";
 const styles = StyleSheet.create({
     touchableHighlight: {
         backgroundColor: 'white',
-        borderRadius: 0.8
+        paddingTop: 2,
+        paddingBottom: 3,
+        paddingLeft: 4,
+        paddingRight: 4,
+        borderRadius: 30,
+        borderWidth: 0,
+    },
+    text: {
+        fontSize: 18,
+        
+        textAlign: 'center',
+        color: 'rgb(30, 108, 147)'
     }
 });
 
@@ -13,22 +24,26 @@ class ToggleButton extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            icon: "+"
+            icon: "+",
+            pressed: false
         };
     } //end constructor()
 
     onPress = () => {
         console.log("ToggleButton pressed");
+        this.setState({
+            pressed: !this.state.pressed
+        });
     }; //onPress()
 
     
-    render () {
+    render() {
         return (
             <TouchableHighlight 
                     style={styles.touchableHighlight}
                     onPress={this.onPress}
                 >
-                <Text>{this.state.icon}</Text>
+                <Text style={styles.text}>{this.state.pressed ? "- " : "+"}</Text>
             </TouchableHighlight>
         );
     }
