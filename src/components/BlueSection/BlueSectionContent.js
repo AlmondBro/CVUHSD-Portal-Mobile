4a4fcfed6fac0bc335dd8ee38274a405200e9e4e
Juan David López Londoño <almondbro@AlmondBros-MacBook-Pro.local>
2019-04-26 00:32:33 -0700

    Got hiding to work

--- src/components/BlueSection/BlueSectionContent.js
+++ src/components/BlueSection/BlueSectionContent.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Button, Linking } from "react-native";
+import { StyleSheet, View, Button, Linking, Animated } from "react-native";
 
 import AppLink from 'react-native-app-link';
 
 import LinkButton from "./../linkButton.js";
 
 const blueSectionContent_styles = StyleSheet.create({
-    blueSectionContent: {
-        
+    blueSection_Content: {
+        height: 500
     }
 });
 
@@ -15,27 +15,49 @@ class BlueSectionContent extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            expanded: this.props.expanded
+            expanded: this.props.expanded,
+            animation: new Animated.Value()
         }
     }
+
+    _setMaxHeight(event){
+        this.setState({
+            maxHeight   : event.nativeEvent.layout.height
+        });
+    }
+
+    _setMinHeight(event){
+        this.setState({
+            minHeight   : event.nativeEvent.layout.height
+        });
+    }
+
+    componentWillReceiveProps(newProps) {
+        if (this.state.expanded !== newProps.expanded) {
+          this.setState({expanded: newProps.expanded});
+        }
+      }
 
     render() {
         return (
             <View style={[
-                    blueSectionContent_styles.blueSection_Content,
-                    { height: this.state.expanded ? 500 : 0}
-                ]}>
+                blueSectionContent_styles.blueSection_Content,
+                    { height: this.state.expanded ? 300 : 0}
+                ]}
+
+                onLayout={this._setMinHeight.bind(this)}
+            >
                     {/* <WebView 
                         style={styles.webView}
                         originWhitelist={['*']}
                         source={{html: "<iframe src='https://www.site24x7.com/sv.do?id=-lTskTIBFC99AjBdJTzdd22ylcZvGBYnfGhcgwvt1-27W89lFFvf7WICSx8TdzUT6kB92hYLWdGYIInKaxcmHcJTzDPBf7IFLjpWmnUEJ18%3D&st=false' scrolling='yes'></iframe>"}}
                         
                     /> */}
-                        <LinkButton 
+                        {/* <LinkButton 
                             name="Spiceworks"
                             imagePath="CVUHSD-Course-Resources.png"
                             deepLink="spiceworks://"
-                        />
+                        /> */}
                         <Button 
                                 onPress={ () => { 
                                                     Linking.openURL("spiceworks://")
