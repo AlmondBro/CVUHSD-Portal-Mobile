7e15e4a91ab69e59ebe054c899e2f91dd935a25b
Juan David Lopez Londono <juandavid@jdlondono.com>
2020-05-15 09:53:24 -0700

    Update package.json, remove extraneous packages

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = function(api) {
   // presets: ["react-native", "babel-preset-expo", "module:metro-react-native-babel-preset", "module:react-native-dotenv", "@babel/preset-env"],
   api.cache(true);
   return {
-    presets: ["module:react-native","react-native", "babel-preset-expo", "module:metro-react-native-babel-preset", "module:react-native-dotenv"],
+    presets: ["babel-preset-expo"],
   };
 };
