5f773050041bd6d404bd2d65d5f89d285ea9ec5e
Juan David Londoño <juandavid@jdlondono.com>
2020-07-09 08:22:41 -0700

    Add  expo clean start script and modifying the Reactrotron  start script to run in web environments.

--- src/config/reactotron.dev.js
+++ src/config/reactotron.dev.js
@@ -1,5 +1,4 @@
-import Reactotron from 'reactotron-react-native';
-import { NativeModules } from 'react-native';
+import { Platform, NativeModules } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 
 import url from 'url';
@@ -7,8 +6,17 @@ import url from 'url';
 const { hostname } = url.parse(NativeModules.SourceCode.scriptURL);
 console.log(hostname); // mine was 192.168.1.2
 
-Reactotron
+const Reactotron = (Platform.OS === "web") ? require('reactotron-react-js') : require('reactotron-react-native');
+
+if (Platform.OS === "web" ) {
+  Reactotron
+  .configure() // we can use plugins here -- more on this later
+  .connect() // let's connect!
+} else {
+  Reactotron
   .setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
   .configure({host:  hostname}) // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
   .connect(); // let's connect!
+}
+
