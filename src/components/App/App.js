619d40492c070c3db859080e6a14253bfb6650c1
AlmondBro <juandavid@jdlondono.com>
2020-10-19 11:03:39 -0700

    Change showUpdate state variable to be false on default.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -51,7 +51,7 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            showUpdate          :   true, 
+            showUpdate          :   false, 
             adUserInfo          :   null,
             appWidth            :   this.props.width,
 
