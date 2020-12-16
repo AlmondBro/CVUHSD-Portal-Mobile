9099efda82e8f2e011509661b07b3c8be8751ce3
AlmondBro <juandavid@jdlondono.com>
2020-12-16 13:39:37 -0800

    Find out why react-native-gesture-handler's

--- src/components/TouchableButton/TouchableButton.js
+++ src/components/TouchableButton/TouchableButton.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Platform } from 'react-native';
 
-import { TouchableOpacity } from 'react-native-gesture-handler';
+// import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Container, Text } from './TouchableButtonStyledComponents';
 
+/*
+    TODO: 
+    Find out why react-native-gesture-handler's 
+    TouchableOpacity does not work in Android. 
+    Conditional requiring is the workaround for 
+    now.
+*/
 const TouchableButton = ({ renderAsStudent, districtPosition, buttonTitle, color, bgColor, children, onPress }) => {
+   let TouchableOpacity;
+
+    if (Platform.OS === "ios") {
+        TouchableOpacity = require("react-native-gesture-handler").TouchableOpacity;
+    } else {
+        TouchableOpacity = require("react-native").TouchableOpacity;
+    }
+
     return (
         <TouchableOpacity
             activeOpacity   =   {0.5}
