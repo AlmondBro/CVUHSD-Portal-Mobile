67266b5c5a017e8956271628a6c0798e6a35e4c4
AlmondBro <juandavid@jdlondono.com>
2021-01-22 11:12:36 -0800

    Implement getUserRequests()in <ViewRequests/>

--- src/components/RequestOptions/ViewRequests/ViewRequests.js
+++ src/components/RequestOptions/ViewRequests/ViewRequests.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import RequestPreview from './RequestPreview/RequestPreview.js';
 
@@ -11,7 +11,46 @@ import { Container, RequestTypeTitle, RequestPreviewContainer } from './ViewRequ
  * @param { string } districtPosition the role of the school district user
  * @param { boolean } renderAsStudent dictates whether a staff member is choosing to view the app through a student's eyes
  */
+
+const isDev = __DEV__;
+
 const ViewRequests = ({navigation, districtPosition, renderAsStudent}) => {
+    let [ isLoading, setIsLoading ] = useState(false);
+
+    const getUserRequests = async (email, requestsType = "All") => {
+        let requests = [];
+        setIsLoading(true);
+
+        const getUserRequests_URL = `${isDev ? "" : "/server"}/helpdesk/request/read/all/user`;
+        const getUserRequests_Headers = {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            "Access-Control-Allow-Credentials": true
+        };
+    
+        let requestsResponse = await fetch(getUserRequests_URL, {
+            method: 'POST',
+            headers: getUserRequests_Headers,
+            body: JSON.stringify({ email, requestsType } )
+        })
+        .then((serverResponse) => serverResponse.json()) //Parse the JSON of the response
+        .then((jsonResponse) => jsonResponse)
+        .catch((error) => {
+            console.error(`Catching error:\t ${error}`);
+        });
+
+        if (requestsResponse && !requestsResponse.error) {
+            requests = requestsResponse.requests;
+            setIsLoading(false);
+        } else {
+            console.log(`Error in fetching the requests.`);
+
+            requests = [];
+        }
+
+        return requests;
+    }; //end getUserRequests
+
     return (
         <Container>
             <RequestTypeTitle
@@ -20,7 +59,7 @@ const ViewRequests = ({navigation, districtPosition, renderAsStudent}) => {
             >
                 All Requests
             </RequestTypeTitle>
-            
+
             <RequestPreviewContainer>
                 <RequestPreview
                     navigation          =   { navigation }
